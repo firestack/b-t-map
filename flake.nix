fc757ead1fffd7649dccba8e78e4064f21cf46f7
Kayla Firestack <git.dev@kaylafire.me>
2023-10-01 22:02:29 -0400

    feat(flake): add mix deps

--- flake.nix
+++ flake.nix
@@ -21,7 +21,20 @@
 				"x86_64-linux"
 			];
 
-			perSystem = { pkgs, ... }: {
+			perSystem = { pkgs, lib, self', ... }: let inherit (pkgs.beam.packages.erlang) fetchMixDeps; in {
+				packages.mixDeps = fetchMixDeps {
+					pname = "mix-deps-btm";
+					src = ./btm;
+					version = "0.0.1";
+					sha256 = "sha256-1rkO0Wl8lIWyB735As/qSTmdkzI991p6G01mA7Nt2yY=";
+				};
+				packages.mixDevDeps = fetchMixDeps {
+					pname = "dev-mix-deps-btm";
+					mixEnv="dev";
+					src = ./btm;
+					version = "0.0.1";
+					sha256 = "sha256-9cxvP5b2TdxPbu8dW8Tu+xD38p04+6I0ux88re4mzj0=";
+				};
 				devshells.default = {
 					packages = [
 						pkgs.elixir
