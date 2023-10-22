90715435c75e021d7ae06d14f189f931cc977cde
Kayla Firestack <kfirestack@mbta.com>
2023-10-22 15:14:16 -0400

    fixup! remove ecto

--- btm/lib/btm_web/endpoint.ex
+++ btm/lib/btm_web/endpoint.ex
@@ -29,7 +29,6 @@ defmodule BtmWeb.Endpoint do
     socket "/phoenix/live_reload/socket", Phoenix.LiveReloader.Socket
     plug Phoenix.LiveReloader
     plug Phoenix.CodeReloader
-    plug Phoenix.Ecto.CheckRepoStatus, otp_app: :btm
   end
 
   plug Plug.RequestId
